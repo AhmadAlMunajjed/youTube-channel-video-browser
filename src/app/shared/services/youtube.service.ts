cc6bfe5554ad8f8086a9e856c4122bd2e0ad126a
Ahmad Almunajjed <ahmadalmunajjed@outlook.com>
2023-10-16 22:36:40 +0800

    add YoutubeService

---
+++ src/app/shared/services/youtube.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment.development';
+
+@Injectable()
+export class YoutubeService {
+
+  //#region varialbes
+  private maxResults = 10;
+  //#endregion
+
+  constructor(
+    private httpClient: HttpClient,
+  ) { }
+
+  getVideos(channelId: string) {
+    return this.httpClient.get(this.getVideosApiUrl(channelId)).toPromise();
+  }
+
+  private getVideosApiUrl(channelId: string): string {
+    return `https://www.googleapis.com/youtube/v3/search?key=${environment.youtubeApiKey}&channelId=${channelId}&part=snippet,id&order=date&maxResults=${this.maxResults}`;
+  }
+
+}
