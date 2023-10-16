13a28e7dabf4dc84bddf54ff243bddb9a1896a10
Ahmad Almunajjed <ahmadalmunajjed@outlook.com>
2023-10-16 22:10:26 +0800

    home page, font

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./pages/home/home.component').then(c => c.HomeComponent)
+  },
+  { path: '**', redirectTo: '' },
+  { path: '', redirectTo: '', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
