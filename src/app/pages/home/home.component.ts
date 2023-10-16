a28ed315777a5f14b9f658911f0228fc87ce0c37
Ahmad Almunajjed <ahmadalmunajjed@outlook.com>
2023-10-16 22:26:12 +0800

    home design and shared module

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { SharedModule } from '@shared/modules';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
+  imports: [
+    SharedModule
+  ],
   standalone: true
 })
 export class HomeComponent {
 
+  videos = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 }
