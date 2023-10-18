a48706fd0de33d1deb1c2493adb863824a2a35ac
Ahmad Almunajjed <ahmadalmunajjed@outlook.com>
2023-10-18 14:01:16 +0800

    add storage service

--- src/app/shared/services/storage.service.ts
+++ src/app/shared/services/storage.service.ts
@@ -6,4 +6,16 @@ import { Injectable } from '@angular/core';
 export class StorageService {
 
   constructor() { }
+
+  // Set a value in storage
+  set(key: string, value: any): void {
+    const serializedValue = JSON.stringify(value);
+    localStorage.setItem(key, serializedValue);
+  }
+
+  // Get a value from storage
+  get(key: string): any {
+    const serializedValue = localStorage.getItem(key);
+    return serializedValue ? JSON.parse(serializedValue) : null;
+  }
 }
